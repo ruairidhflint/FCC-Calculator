0c8179f647acf84f9c1e77923aab8b8d7d129b68
Rory Flint <roryflintphoto@gmail.com>
2020-04-03 17:11:57 +0100

    Add logic to display digits + decimal places etc

--- src/Components/CalcContainer.js
+++ src/Components/CalcContainer.js
@@ -4,17 +4,31 @@ import Display from './Display';
 import ButtonContainer from './ButtonContainer';
 
 function CalcContainer() {
-    const [currentInput, setCurrentInput] = useState('0');
+  const [currentInput, setCurrentInput] = useState('0');
 
-    const inputNumber = (newDigit) => {
+  const inputNumber = newDigit => {
+    if (currentInput === '0' && newDigit === '.') {
+      setCurrentInput('0.');
+    } else if (currentInput === '0') {
+      setCurrentInput(newDigit);
+    } else if (currentInput.length >= 8) {
+      setCurrentInput(currentInput);
+    } else if (newDigit === '.') {
+      if (currentInput.indexOf('.') === -1) {
         setCurrentInput(currentInput + newDigit);
+      } else {
+        setCurrentInput(currentInput);
+      }
+    } else {
+      setCurrentInput(currentInput + newDigit);
     }
-    return (
+  };
+  return (
     <div className="container">
-        <Display currentInput={currentInput} />
-        <ButtonContainer numberInput={inputNumber}/>
+      <Display currentInput={currentInput} />
+      <ButtonContainer numberInput={inputNumber} />
     </div>
-    )
+  );
 }
 
 export default CalcContainer;
