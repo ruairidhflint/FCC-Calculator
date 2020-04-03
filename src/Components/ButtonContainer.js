92a3e8bb012cdf30d35785b1191643015e106a5e
Rory Flint <roryflintphoto@gmail.com>
2020-04-03 17:48:18 +0100

    Progress on calculator logic

--- src/Components/ButtonContainer.js
+++ src/Components/ButtonContainer.js
@@ -12,20 +12,20 @@ function ButtonContainer(props) {
           type="horizontal"
           value="Clear"
         />
-        <Button id="multiply" type="reg" value="X" />
-        <Button id="divide" type="reg" value="%" />
+        <Button id="multiply" input={props.operatorInput} type="reg" value="x" />
+        <Button id="divide" input={props.operatorInput} type="reg" value="%" />
       </div>
       <div className="button-row">
         <Button id="seven" input={props.numberInput} type="reg" value="7" />
         <Button id="eight" input={props.numberInput} type="reg" value="8" />
         <Button id="nine" input={props.numberInput} type="reg" value="9" />
-        <Button id="add" type="reg" value="+" />
+        <Button id="add" input={props.operatorInput} type="reg" value="+" />
       </div>
       <div className="button-row">
         <Button id="four" input={props.numberInput} type="reg" value="4" />
         <Button id="five" input={props.numberInput} type="reg" value="5" />
         <Button id="six" input={props.numberInput} type="reg" value="6" />
-        <Button id="subtract" type="reg" value="-" />
+        <Button id="subtract"  input={props.operatorInput}type="reg" value="-" />
       </div>
       <div className="big-row">
         <div className="big-row-left">
--- src/Components/CalcContainer.js
+++ src/Components/CalcContainer.js
@@ -5,19 +5,27 @@ import ButtonContainer from './ButtonContainer';
 
 function CalcContainer() {
   const [currentInput, setCurrentInput] = useState('0');
+  const [currentSum, setCurrentSum] = useState('');
 
   const inputNumber = newDigit => {
-    if (currentInput === '0' && newDigit === '.') {
+    if (
+      currentInput === '+' ||
+      currentInput === '-' ||
+      currentInput === 'x' ||
+      currentInput === '%'
+    ) {
+      setCurrentInput(newDigit);
+    } else if (currentInput === '0' && newDigit === '.') {
       setCurrentInput('0.');
     } else if (currentInput === '0') {
       setCurrentInput(newDigit);
     } else if (currentInput.length >= 8) {
-      setCurrentInput(currentInput);
+      return null;
     } else if (newDigit === '.') {
       if (currentInput.indexOf('.') === -1) {
         setCurrentInput(currentInput + newDigit);
       } else {
-        setCurrentInput(currentInput);
+        return null;
       }
     } else {
       setCurrentInput(currentInput + newDigit);
@@ -26,11 +34,34 @@ function CalcContainer() {
 
   const clear = () => {
     setCurrentInput('0');
+    setCurrentSum('');
   };
+
+  const inputOperator = newOperator => {
+    if (
+      currentInput === '+' ||
+      currentInput === '-' ||
+      currentInput === 'x' ||
+      currentInput === '%'
+    ) {
+      return null;
+    }
+    setCurrentSum(
+      currentSum
+        ? currentSum + ' ' + currentInput + ' ' + newOperator
+        : currentInput + ' ' + newOperator,
+    );
+    setCurrentInput(newOperator);
+  };
+
   return (
     <div className="container">
-      <Display currentInput={currentInput} />
-      <ButtonContainer numberInput={inputNumber} clear={clear} />
+      <Display currentInput={currentInput} currentSum={currentSum} />
+      <ButtonContainer
+        numberInput={inputNumber}
+        clear={clear}
+        operatorInput={inputOperator}
+      />
     </div>
   );
 }
